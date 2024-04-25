384aaabbe48752dfc040f2ff356ad02c120c7d76
MelanieCoentreiras <melanie.coentreiras@gmail.com>
2024-04-25 12:47:59 +0200

    components added

---
+++ src/SayHello.jsx
@@ -0,0 +1,9 @@
+export function SayHello() {
+  return (
+    <div style={{ border: "1px solid red" }}>
+      <p>SayHelloComponent</p>
+      <h1>Bonjour!</h1>
+      <p>Hello Mel!</p>
+    </div>
+  );
+}
---
+++ src/Tweet.jsx
@@ -0,0 +1,9 @@
+export function Tweet() {
+  return (
+    <div className='tweet'>
+      <h3>Name</h3>
+      <p>content</p>
+      <p>12 🤌</p>
+    </div>
+  );
+}
